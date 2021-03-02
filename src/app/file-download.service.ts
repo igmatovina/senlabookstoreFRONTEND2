eca952ef0f5771874487e0b31df65b0ca864d3d1
igmatovina <igmatovina@gmail.com>
2021-03-02 02:09:30 +0100

    trying to make download functional

--- src/app/file-download.service.ts
+++ src/app/file-download.service.ts
@@ -1,5 +1,7 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams, HttpRequest, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
@@ -9,15 +11,74 @@ export class FileDownloadService {
   constructor(private http:HttpClient) { }
 
   
-  downloadFile(data: any) {
+  downloadFile2(data: any) {
     // we would call the spring-boot service
+    const REQUEST_HEADERS = new HttpHeaders({ 'Accept': 'application/xml' });
     const REQUEST_PARAMS = new HttpParams().set('fileName', data.fileName);
-    const REQUEST_URI = 'http://localhost:8080/convertToXml2';
+    const REQUEST_URI = 'http://localhost:8080/download';
     return this.http.get(REQUEST_URI, {
+      headers : REQUEST_HEADERS,
       params: REQUEST_PARAMS,
-      responseType: 'arraybuffer'
+      responseType: 'blob'
     })
   }
+
+  downloadFileSystem(filename: string): Observable<HttpResponse<string>> {
+    let headers = new HttpHeaders();
+    headers = headers.append('Accept', 'text/csv; charset=utf-8');
+    return this.http.get('http://localhost:8080/download2' + filename, {
+      headers: headers,
+      observe: 'response',
+      responseType: 'text'
+    });
+  }
+
+  downloadFile3() {
+		return this.http.get('http://localhost:8080/download');
+  }
+
+  
+  downloadFile4() {
+    const REQUEST_HEADERS = new HttpHeaders({ 'Accept': 'application/xml' });
+    const REQUEST_URI = 'http://localhost:8080//downloadXml';
+    return this.http.get(REQUEST_URI, {
+      headers : REQUEST_HEADERS,
+      responseType: 'text'
+    })  
+  }
+
+  public getPDF(): Observable<Blob> {   
+    //const options = { responseType: 'blob' }; there is no use of this
+        let uri = 'http://localhost:8080//downloadXml';
+        // this.http refers to HttpClient. Note here that you cannot use the generic get<Blob> as it does not compile: instead you "choose" the appropriate API in this way.
+        return this.http.get(uri, { responseType: 'blob' });
+    }
+
+
+  downloadYoutube(data:any){
+
+    const REQUEST_HEADERS = new HttpHeaders({ 'Accept': 'application/xml' });
+    const REQUEST_PARAMS = new HttpParams().set('fileName', data.fileName);
+    const REQUEST_URI = 'http://localhost:8080/downloadYoutube';
+    return this.http.get(REQUEST_URI, {
+      headers : REQUEST_HEADERS,
+      params: REQUEST_PARAMS,
+      responseType: 'blob'
+    })
+
+  }
+/*   pushFileToStorage(file: File): Observable<HttpEvent<{}>> {
+    const data: FormData = new FormData();
+    data.append('file', file);
+    const newRequest = new HttpRequest('POST', 'http://localhost:8080/savefile', data, {
+    reportProgress: true,
+    responseType: 'text'
+    });
+    return this.http.request(newRequest);
+}}
+ */
+
+
 
 
 }
--- src/app/search-delete/search-delete.component.ts
+++ src/app/search-delete/search-delete.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { BookAddService } from '../book-add.service';
 import { FileDownloadService } from '../file-download.service';
 import { saveAs } from 'file-saver';
+import * as fileSaver from 'file-saver';
 
 const MIME_TYPES : any = {
   pdf: 'application/pdf',
@@ -24,8 +25,9 @@ export class SearchDeleteComponent implements OnInit {
 
   books:any;
   title!: string;
-  
-  constructor(private service: BookAddService ,private service2: FileDownloadService, private router: Router, private http:HttpClient) { }
+  fileSystemName!: string;
+
+  constructor(private service: BookAddService ,private fileDownloadService: FileDownloadService, private router: Router, private http:HttpClient) { }
 
 
 
@@ -49,13 +51,42 @@ export class SearchDeleteComponent implements OnInit {
   }
 
 
-  downloadFile() {
-    const EXT = this.xmlFileName.substr(this.xmlFileName.lastIndexOf('.') + 1);
-    this.service2.downloadFile({ 'fileName': this.xmlFileName})
-    .subscribe(data => {
-      //save it on the client machine.
-      saveAs(new Blob([data], {type: MIME_TYPES[EXT]}), this.xmlFileName);
-    })
+  downloadFile2() {
+    this.fileDownloadService.downloadFile4();
   }
+
+
+  public downloadYoutube(fileName:any){
+    const EXT = fileName.substr(fileName.lastIndexOf('.') + 1);
+    this.fileDownloadService.downloadYoutube({'fileName':fileName})
+    .subscribe(data=>{
+      saveAs(new Blob([data], { type:MIME_TYPES[EXT]}),fileName);
+
+    })
+
+  }
+    /*   downloadFile() {
+    this.service2.downloadFile2().subscribe((response: any) => { //when you use stricter type checking
+			let blob:any = new Blob([response], { type: 'application/xml; charset=utf-8' });
+			const url = window.URL.createObjectURL(blob);
+			//window.open(url);
+			//window.location.href = response.url;
+			fileSaver.saveAs(blob, 'booklist.xml');
+		//}), error => console.log('Error downloading the file'),
+		}), (error: any) => console.log('Error downloading the file'), //when you use stricter type checking
+                 () => console.info('File downloaded successfully');
+  }
+
+  download3() {
+    this.service2.downloadFile3().subscribe((response: any) => { //when you use stricter type checking
+			let blob:any = new Blob([response], { type: 'text/json; charset=utf-8' });
+			const url = window.URL.createObjectURL(blob);
+			//window.open(url);
+			//window.location.href = response.url;
+			fileSaver.saveAs(blob, 'employees.json');
+		//}), error => console.log('Error downloading the file'),
+		}), (error: any) => console.log('Error downloading the file'), //when you use stricter type checking
+                 () => console.info('File downloaded successfully');
+  } */
 
 }
